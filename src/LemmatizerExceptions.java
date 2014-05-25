be5f8a334d8b625ce6322b96479ad1fc398f8cf1
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2014-05-25 21:23:34 +0200

    Better exceptions logging

--- src/LemmatizerExceptions.java
+++ src/LemmatizerExceptions.java
@@ -1,19 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
-import java.util.logging.Logger;
 
 /**
  *
  * @author arek
  */
 public abstract class LemmatizerExceptions extends Exception{
-    protected Integer excType;
+    private Integer excType;
+    private Integer code;
     
     public LemmatizerExceptions(Integer code, String msg) {
         super(msg);
+        this.excType = excType;
+        this.code = code;
+        this.log();
     }
     
-    public Integer getExcType() {
-        return this.excType;
+    private void log() {
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(new BufferedWriter(new FileWriter("logs/exception.log", true)));
+        } catch (Exception ex) {
+            return;
+        }
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        pw.println(dateFormat.format(cal.getTime())+" "+this+" (code: "+this.code+")");
+        this.printStackTrace(pw);
+        pw.println();
+        pw.close();
     }
     
 }
