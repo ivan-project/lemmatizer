f185d7163014b06b62627eb7ce1d0a3bd88302bf
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2014-06-08 22:14:19 +0200

    unnecessary rm

--- run.sh
+++ run.sh
@@ -8,8 +8,6 @@ if [ ! -d ./tmp ]; then
   mkdir -p ./tmp;
 fi
 
-rm *.class 2>/dev/null
-
 javac -classpath ".:mongo-2.10.1.jar" *.java
 
 java -Dfile.encoding=UTF-8 -classpath ".:mongo-2.10.1.jar" Lemmatizer $@
