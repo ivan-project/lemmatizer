ff200220addf17029122f55be553107926e8722f
eXtreme <jacek.jedrzejewski@gmail.com>
2014-06-17 23:40:58 +0200

    removed javac and rm commands from run.sh (sic)

--- run.sh
+++ run.sh
@@ -8,9 +8,4 @@ if [ ! -d ./tmp ]; then
   mkdir -p ./tmp;
 fi
 
-javac -classpath ".:mongo-2.10.1.jar" *.java
-
 java -Dfile.encoding=UTF-8 -classpath ".:mongo-2.10.1.jar" Lemmatizer $@
-
-rm *.class 2>/dev/null
-
